b8aff28a30a52844a881260c81e1ed8545b1e5f7
Ben Farr <ben.farr@marmalade.co.uk>
2020-10-22 14:06:53 +0100

    Updates

---
+++ precache-manifest.eea10790004d7b09a2f3a551ae4cceb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0da26459839b8c0e5b89",
+    "url": "/address-book/static/css/main.939b8b54.chunk.css"
+  },
+  {
+    "revision": "0da26459839b8c0e5b89",
+    "url": "/address-book/static/js/main.aaa6fd15.chunk.js"
+  },
+  {
+    "revision": "7d5f1059a20d6079c903",
+    "url": "/address-book/static/js/runtime~main.7fd9516f.js"
+  },
+  {
+    "revision": "75094b086d070d2f7684",
+    "url": "/address-book/static/js/2.54a2fe0a.chunk.js"
+  },
+  {
+    "revision": "8099a4d9d0c48ed5271c36b9f34210f1",
+    "url": "/address-book/index.html"
+  }
+];
